383948094c12ebe11d4113e21a8333cedc28d709
N0E-P <pecci.noe@gmail.com>
2022-12-13 14:41:13 +0100

    [WORKING] see the eth balance of an address

--- commands/getData.js
+++ commands/getData.js
@@ -1,3 +1,10 @@
+const { ethers } = require("ethers");
+const INFURA_KEY = process.env.INFURA_KEY
+const CURRENT_NETWORK = process.env.CURRENT_NETWORK
+const provider = new ethers.providers.JsonRpcProvider(`https://${CURRENT_NETWORK}.infura.io/v3/${INFURA_KEY}`);
+
+const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+
 module.exports = {
   category: "The Blockchain Adapter commands",
   description: "Get blockchain Data",
@@ -6,7 +13,11 @@ module.exports = {
 
     console.log("getData function starting...");
     channel.send("**Hi " + message.author.username + "!** I'm Starting... :grin:");
-    
+
+    const balance = await provider.getBalance(address)
+    const msg = ethers.utils.formatEther(balance);
+    channel.send(msg);
+
     return;
   }
 }
