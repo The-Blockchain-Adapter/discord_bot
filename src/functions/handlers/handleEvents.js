39da4cea1e3d1e7820b1bbb211d29c09fd188c4d
N0E-P <pecci.noe@gmail.com>
2022-12-16 15:44:05 +0100

    mongoDB don't connect yet

--- src/functions/handlers/handleEvents.js
+++ src/functions/handlers/handleEvents.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { connection } = require("mongoose");
 
 // Honnetement j'ai pas compris a quoi servais ce fichier
 module.exports = (client) => {
@@ -17,6 +18,18 @@ module.exports = (client) => {
 						else client.on(event.name, (...args) => event.execute(...args, client));
 					}
 					break;
+
+				case "mongo":
+					for (const file of eventFiles) {
+						const event = require(`../../events/${folder}/${file}`);
+						if (event.once)
+							connection.once(event.name, (...args) =>
+								event.execute(...args, client)
+							);
+						else connection.on(event.name, (...args) => event.execute(...args, client));
+					}
+					break;
+
 				default:
 					break;
 			}
