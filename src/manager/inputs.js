0a14882eea3ea59f1130015966a50756c1d815a7
N0E-P <pecci.noe@gmail.com>
2023-01-31 11:57:45 +0100

    type verifications

--- src/manager/inputs.js
+++ src/manager/inputs.js
@@ -15,7 +15,7 @@ module.exports = {
 				inputs.push(Boolean(input));
 
 				// Detect if the input string is a hex string
-			} else if (input.startsWith("0x") || input.startsWith("0X")) {
+			} else if (input?.startsWith("0x") || input?.startsWith("0X")) {
 				inputs.push(input);
 
 				// Detect if the input string is a number, then convert it to a number
@@ -37,8 +37,27 @@ module.exports = {
 			dataOrAction?.text,
 		];
 		for (var i = 0; i < objects.length; i++) {
-			if (objects[i]?.length == 1 && objects[i].match(/[A-Z]/i)) {
-				objects[i] = scriptInputs[objects[i].charCodeAt(0) - 65];
+			var object = objects[i];
+
+			if (object?.length == 1 && object.match(/[A-Z]/i)) {
+				object = scriptInputs[object.charCodeAt(0) - 65];
+			}
+
+			// Detect if the object string is a boolean, then convert it to a boolean
+			if (object == "true" || object == "false") {
+				objects[i] = Boolean(object);
+
+				// Detect if the object string is a hex string
+			} else if (object?.startsWith("0x") || object?.startsWith("0X")) {
+				objects[i] = object;
+
+				// Detect if the object string is a number, then convert it to a number
+			} else if (!isNaN(object)) {
+				objects[i] = Number(object);
+
+				// Else, it's a string
+			} else {
+				objects[i] = object;
 			}
 		}
 
