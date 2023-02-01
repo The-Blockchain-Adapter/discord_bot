d5ed79d6cb3c494c88d6d28e3a9b2cab5b49cc9f
N0E-P <pecci.noe@gmail.com>
2023-02-01 16:53:08 +0100

    the previous one wasn't working. this one yes

--- src/manager/inputs.js
+++ src/manager/inputs.js
@@ -8,8 +8,11 @@ module.exports = {
 			let input = plainDataOrAction.inputs[i];
 
 			// Detect if the input string is a letter, then convert it to a normal input
-			if (input.length == 3 && input.match(/#([A-Z])#/i)) {
-				input = scriptInputs[input.charCodeAt(0) - 65];
+			if (input.length == 3 && input.match(/#([A-Z])#/g)) {
+				// retrieve the letter string from the string with #
+				input.replace(/#([A-Z])#/g, (match, letter) => letter);
+				// Get the input from the script inputs
+				input = scriptInputs[letter.charCodeAt(0) - 65];
 			}
 
 			// Detect if the input string is a boolean, then convert it to a boolean
@@ -32,8 +35,11 @@ module.exports = {
 
 		// Replace the letter objects with the correct input
 		Object.entries(plainDataOrAction).forEach(([key, value]) => {
-			if (key != "inputs" && value?.length == 3 && value.match(/#([A-Z])#/i)) {
-				plainDataOrAction[key] = scriptInputs[value.charCodeAt(0) - 65];
+			if (key != "inputs" && value?.length == 3 && value.match(/#([A-Z])#/g)) {
+				// retrieve the letter string from the string with #
+				const letter = value[1];
+				// replace the letter object with the correct input
+				plainDataOrAction[key] = scriptInputs[letter.charCodeAt(0) - 65];
 			}
 		});
 
